8b787256add830a26921d2b4b7a62ac32871fead
songdexin <2523765999@qq.com>
2020-06-11 15:29:54 +0800

    修复N+1的问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+//		$topics = Topic::paginate();//默认是15条的
+		$topics = Topic::with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
