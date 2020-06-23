b30284df5bf3bd111b995e764928e571200a74c4
songdexin <2523765999@qq.com>
2020-06-23 12:18:54 +0800

    新增编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
   <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  @yield('styles')
 </head> <body>
 <div id="app" class="{{ route_class() }}-page">
   @include('layouts._header')
@@ -22,5 +23,6 @@
 </div>
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
