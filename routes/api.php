a095fffc2caf34c6949cc00662d4ecf03ce5ff5a
songdexin <2523765999@qq.com>
2020-07-23 19:14:04 +0800

    3.5 频率限制

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,17 @@ use Illuminate\Http\Request;
 
 
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
-    //短信验证码
-    Route::post('verificationCodes', 'verificationCodesController@store')->name('verificationCodes.store');
-    //用户注册
-    Route::post('users', 'UsersController@store')->name('users.store');
+//Route::prefix('v1')->namespace('Api')->name('api.v1.')->middleware('throttle:1,1')->group(function() {#增加频率限制
+    
+    Route::middleware('thorttle' . config('api.rate_limits.sign'))->group(function () {
+        //短信验证码
+        Route::post('verificationCodes', 'verificationCodesController@store')->name('verificationCodes.store');
+        //用户注册
+        Route::post('users', 'UsersController@store')->name('users.store');
+    });
+
+    Route::middleware('throttle' . config('api.rate_limits.access'))->group(function() {
+
+    });
+
 });
