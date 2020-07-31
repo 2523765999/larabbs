a5b67725a3344ce11457fa62cb2755931a4006c8
songdexin <2523765999@qq.com>
2020-07-31 20:58:19 +0800

    5.2 不全

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\UserRequest;
 use App\Http\Resources\UserResource;
+use App\Models\Image;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
@@ -41,5 +42,22 @@ class UsersController extends Controller
     {
 //        return new UserResource($request->user())->showSensitiveFields();
         return (new UserResource($request->user()))->showSensitiveFields();//开关设计的很巧妙
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $request->user();
+        $attributes = $request->only(['name', 'email', 'introduction']);
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+            $attributes['avatar'] = $image->path;
+        }
+        echo $attributes['avatar'];
+//        $attributes['avatar'] = '';
+//        dump($user,$attributes);
+//        dump($attributes);
+//        dd($user);
+        $user->update($attributes);
+        return (new UserResource($user))->showSensitiveFields();
     }
 }
