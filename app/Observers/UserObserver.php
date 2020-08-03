99251fdf2a9569ca093c60ecb6429f92e6f930b6
songdexin <2523765999@qq.com>
2020-08-03 13:09:03 +0800

    5.2 编辑用户信息

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -21,6 +21,8 @@ class UserObserver
 
     public function saving(User $user)
     {
-        $user->avatar = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593527892738&di=c0196e15545bffbce5a974de017c6e41&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fpkm%2Fpk%2F13.jpg';
+        if (!$user->avatar) {
+            $user->avatar = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593527892738&di=c0196e15545bffbce5a974de017c6e41&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fpkm%2Fpk%2F13.jpg';
+        }
     }
 }
