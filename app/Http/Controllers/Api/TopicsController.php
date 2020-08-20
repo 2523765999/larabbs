fcad40e0b1dbf63ffcd8306e837e81952fffd369
songdexin <2523765999@qq.com>
2020-08-20 14:28:19 +0800

    6.6 话题详情

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Queries\TopicQuery;
 use App\Http\Requests\Api\TopicRequest;
 use App\Http\Resources\TopicResource;
 use App\Models\Topic;
@@ -37,43 +38,68 @@ class TopicsController extends Controller
         return response(null, 204);
     }
 
-    public function index(Request $request, Topic $topic)
+//    public function index(Request $request, Topic $topic)
+    public function index(Request $request, TopicQuery $query)
     {
-//        dd($topic);
-        $query = $topic->query();
-//        dd($query);
-
-        if ($categoryId = $request->category_id) {
-            $query->where('category_id', $categoryId);
-        }
+//        $query = $topic->query();
+//
+//        if ($categoryId = $request->category_id) {
+//            $query->where('category_id', $categoryId);
+//        }
+//        $topics = Topic::withOrder($query, $request->order)->paginate();
+//        -------------------------------------------------------------------------------------------------------
 //        $topics = $query
 //            ->with('user', 'category')
 //            ->withOrder($request->order)
 //            ->paginate();
-        $topics = QueryBuilder::for(Topic::class)
-            ->allowedIncludes('user', 'category')//allowedIncludes 方法传入可以被 include 的参数
-            ->allowedFilters([//控制可用的搜索条件
-                'title',//模糊搜索
-                AllowedFilter::exact('category_id'),//category_id 是精确搜索
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])//使用 filter 参数可以进行搜索，该参数是个数组
-            ->paginate();
-//        dd($topics);
-//        $topics = Topic::withOrder($query, $request->order)->paginate();
+//        -------------------------------------------------------------------------------------------------------
+//        $topics = QueryBuilder::for(Topic::class)
+//            ->allowedIncludes('user', 'category')//allowedIncludes 方法传入可以被 include 的参数
+//            ->allowedFilters([//控制可用的搜索条件
+//                'title',//模糊搜索
+//                AllowedFilter::exact('category_id'),//category_id 是精确搜索
+//                AllowedFilter::scope('withOrder')->default('recentReplied'),
+//            ])//使用 filter 参数可以进行搜索，该参数是个数组
+//            ->paginate();
+//        -------------------------------------------------------------------------------------------------------
+        $topics = $query->paginate();
         return  TopicResource::collection($topics);
     }
 
-    public function userIndex(Request $request, User $user)
+    /**
+     * @desc 某个用户发布的话题
+     * @param Request $request
+     * @param User $user
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+//    public function userIndex(Request $request, User $user)
+    public function userIndex(Request $request, User $user, TopicQuery $query)
     {
-        $query = $user->topics()->getQuery();
-        $topics = QueryBuilder::for($query)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+//        $query = $user->topics()->getQuery();
+//        $topics = QueryBuilder::for($query)
+//            ->allowedIncludes('user', 'category')
+//            ->allowedFilters([
+//                'title',
+//                AllowedFilter::exact('category_id'),
+//                AllowedFilter::scope('withOrder')->default('recentReplied'),
+//            ])
+//            ->paginate();
+//        -------------------------------------------------
+        $topics = $query->where('user_id', $user->id)->paginate();
         return TopicResource::collection($topics);
+    }
+
+//    public function show(Topic $topic)
+//    public function show($topicId)
+    public function show($topicId, TopicQuery $query)
+    {
+//        return TopicResource::collection($topic);
+//        ----------------------------------------------------------
+//        $topics = QueryBuilder::for(Topic::class)
+//            ->allowedIncludes('user', 'category')
+//            ->findOrFail($topicId);
+//        ----------------------------------------------------------
+        $topics = $query->findOrFail($topicId);
+        return new TopicResource($topics);
     }
 }
